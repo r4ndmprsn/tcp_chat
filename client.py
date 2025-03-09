18b515ec62bd9270dea54cb0aa1686b0bfbf406c
r4ndmprsn <piotr.p.brodowski@gmail.com>
2025-03-09 20:08:45 +0100

    Update client.py
    - users can see each other's messages now

--- client.py
+++ client.py
@@ -1,17 +1,29 @@
 import socket
+import threading
 
-server_ip = input("server ip: ")
-server_port = int(input("server port: "))
+def server_connection(server_ip, server_port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, server_port))
+    recive = client_socket.recv(1024).decode()
+    if recive == "succesfully connected":
+        recive_thread = threading.Thread(target=recive_messages, args=(client_socket,), daemon=True)
+        recive_thread.start()
+        while True:
+            msg = input()
+            if msg.lower() == "exit":
+                print("Closing connection.")
+                break
+            client_socket.sendall(msg.encode())
+    else:
+        print("connection failed")
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((server_ip, server_port))
-recive = client_socket.recv(1024).decode()
-if recive == "succesfully connected":
+def recive_messages(client_socket):
     while True:
-        msg = input("send something: ")
-        if msg.lower() == "exit":
-            print("Closing connection.")
+        rcv_msg = client_socket.recv(1024).decode()
+        if not rcv_msg:
             break
-        client_socket.sendall(msg.encode())
-else:
-    print("connection failed")
+        print(rcv_msg)
+if __name__ == "__main__":
+    server_ip = input("server ip: ")
+    server_port = int(input("server port: "))
+    server_connection(server_ip, server_port)
